73b9c8bede96bd66274e0965f7528c101ab4154b
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-03-22 20:47:17 +0500

    Completed Task 6

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,44 +1,50 @@
 const menu = () => {
-    const menuIcon = document.querySelector('.menu__icon'),
-        menu = document.querySelector('.popup-dialog-menu'),
+    const menu = document.querySelector('.popup-dialog-menu'),
         width = document.documentElement.clientWidth,
         height = document.documentElement.clientHeight,
         body = document.querySelector('body');
 
-    // opening the menu
-    menuIcon.addEventListener('click', () => {
-        if (width >= 576) {
-            menu.style.right = '645px';
-        } else {
-            menu.style.top = `${height}px`;
-        }
-    });
-
-    const scrollToBlock = (target, event) => {
+    const scrollToBlock = (link, event) => {
         event.preventDefault();
 
-        const link = target.firstChild,
-            idBlock = link.getAttribute('href').substring(1),
+        const idBlock = link.getAttribute('href').substring(1),
             block = document.getElementById(idBlock);
 
-        block.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        if (idBlock) {
+            block.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
     };
 
     // go to the block
     body.addEventListener('click', (event) => {
         const target = event.target;
 
-        if (target.closest('.popup-menu-nav__item')) {
+        // open the menu
+        if (target.closest('.menu__icon')) {
+            if (width >= 576) {
+                menu.style.right = '645px';
+            } else {
+                menu.style.top = `${height}px`;
+            }
+        }
+
+        const closeMenu = () => {
             if (width >= 576) {
                 menu.style.right = '0px';
             } else {
                 menu.style.top = '0px';
             }
-            scrollToBlock(target.closest('.popup-menu-nav__item'), event);
+        };
+
+        if (target.closest('.close-menu')) { closeMenu(); }
+
+        if (target.closest('.menu-link')) {
+            closeMenu();
+            scrollToBlock(target.closest('.menu-link'), event);
         }
 
         if (target.closest('.button-footer')) {
-            scrollToBlock(target.closest('.button-footer'), event);
+            scrollToBlock(target.closest('.button-footer').firstChild, event);
         }
     });
 };
--- src/modules/popups.js
+++ src/modules/popups.js
@@ -1,10 +1,26 @@
 const popups = () => {
-    // const
+    const body = document.querySelector('body'),
+        repairTypesSection = document.querySelector('.popup-repair-types');
 
-    // link-list-menu
-    // 
-    // popup popup-repair-types
+    body.addEventListener('click', (event) => {
+        const target = event.target;
 
+        // if (!target.closest('.popup-dialog-repair-types') && getComputedStyle(repairTypesSection).visibility === 'visible') {
+        //     repairTypesSection.style.visibility = 'hidden';
+        // }
+
+        if (target.closest('.link-list-menu') || target.closest('.link-list-repair')) {
+            if (target.closest('a')) {
+                event.preventDefault();
+                repairTypesSection.style.visibility = 'visible';
+            }
+        }
+
+        if (target.closest('.popup-repair-types') && target.closest('.close')) {
+            repairTypesSection.style.visibility = 'hidden';
+        }
+
+    });
 };
 
 export default popups;
