6fa4e859d859ab68033bfb1db8f714f53343e7a7
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-03-22 21:17:54 +0500

    Completed Task 7

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@
 import showNumbers from './modules/showNumbers';
 import menu from './modules/menu';
 import popups from './modules/popups';
+import feedback from './modules/feedback';
 
 
 showNumbers();
 menu();
 popups();
+feedback();
