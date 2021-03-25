bf614e3e60fc4b61a368d1a3aa1ff82c76ff5223
Ivanov Dmitriy <rey.veritas@yandex.ru>
2021-03-25 22:40:32 +0500

    Completed Task 14

--- src/modules/transparency.js
+++ src/modules/transparency.js
@@ -1,42 +1,83 @@
 const transparency = () => {
-    const transparencySection = document.getElementById('transparency'),
-        transparencySlides = document.querySelectorAll('.transparency-item');
+    const body = document.querySelector('body'),
+        transparencySection = document.getElementById('transparency'),
+        transparencySlides = document.querySelectorAll('.transparency-item'),
+        popupTransparency = document.querySelector('.popup-transparency'),
+        popupTransparencySlides = document.querySelectorAll('.popup-transparency-slider__slide'),
+        transparencyPopupCounter = document.getElementById('transparency-popup-counter');
 
     let count = 0;
 
-    transparencySection.addEventListener('click', (event) => {
+    body.addEventListener('click', (event) => {
         const target = event.target;
 
-        const checkFlex = () => {
-            transparencySlides.forEach((item, index) => {
-                if (item.style.display === 'flex') {
-                    item.style.display = 'none';
+        const searchDisplayState = (arr, displayType) => {
+            arr.forEach((item, index) => {
+                if (item.style.display === displayType) {
                     count = index;
                 }
             });
         };
 
-        if (target.closest('#transparency-arrow_left')) {
-            checkFlex();
-
+        const toLeft = (arrSlides, displayType) => {
+            searchDisplayState(arrSlides, displayType);
+            arrSlides[count].style.display = 'none';
             if (count === 0) {
-                transparencySlides[transparencySlides.length - 1].style.display = 'flex';
+                arrSlides[arrSlides.length - 1].style.display = displayType;
             } else {
-                transparencySlides[count - 1].style.display = 'flex';
+                arrSlides[count - 1].style.display = displayType;
+            }
+        };
+
+        const toRight = (arrSlides, displayType) => {
+            searchDisplayState(arrSlides, displayType);
+            arrSlides[count].style.display = 'none';
+            if (count === arrSlides.length - 1) {
+                arrSlides[0].style.display = displayType;
+            } else {
+                arrSlides[count + 1].style.display = displayType;
+            }
+        };
+
+        if (target.closest('#transparency')) {
+            if (target.closest('#transparency-arrow_left')) {
+                toLeft(transparencySlides, 'flex');
+            }
+
+            if (target.closest('#transparency-arrow_right')) {
+                toRight(transparencySlides, 'flex');
+            }
+
+            if (target.closest('.transparency-item')) {
+                popupTransparency.style.visibility = 'visible';
+                searchDisplayState(transparencySlides, 'flex');
+                popupTransparencySlides.forEach(item => item.style.display = 'none');
+                popupTransparencySlides[count].style.display = 'block';
+                transparencyPopupCounter.querySelector('.slider-counter-content__total').textContent = popupTransparencySlides.length;
+                transparencyPopupCounter.querySelector('.slider-counter-content__current').textContent = count + 1;
             }
         }
 
-        if (target.closest('#transparency-arrow_right')) {
-            checkFlex();
+        if (target.closest('.popup-transparency')) {
+            if (target.closest('.close')) {
+                popupTransparency.style.visibility = 'hidden';
+            }
 
-            if (count === transparencySlides.length - 1) {
-                transparencySlides[0].style.display = 'flex';
-            } else {
-                transparencySlides[count + 1].style.display = 'flex';
+            if (target.closest('#transparency_left')) {
+                toLeft(popupTransparencySlides, 'block');
+                searchDisplayState(popupTransparencySlides, 'block');
+                transparencyPopupCounter.querySelector('.slider-counter-content__current').textContent = count + 1;
+            }
+
+            if (target.closest('#transparency_right')) {
+                toRight(popupTransparencySlides, 'block');
+                searchDisplayState(popupTransparencySlides, 'block');
+                transparencyPopupCounter.querySelector('.slider-counter-content__current').textContent = count + 1;
             }
         }
-
     });
+
+
 
     const resizeSlide = () => {
         if (transparencySection.clientWidth <= 1090) {
